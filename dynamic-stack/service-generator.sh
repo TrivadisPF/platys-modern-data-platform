5be1fd130db1bc72cc44729a0f7bbc4e69028594
Guido Schmutz <guido.schmutz@trivadis.com>
2019-12-16 16:53:06 +0100

    Update to also accept the version of the docker image and solve permission problem

--- dynamic-stack/service-generator.sh
+++ dynamic-stack/service-generator.sh
@@ -3,13 +3,13 @@
 args=("$@")
 len=${#args[@]}
 
-if [ $len = 2 ] #2 arguments are required
+if [ $len = 3 ] #3 arguments are required
 then
-   docker run --rm -v ${args[0]}:/tmp/custom-values.yml -v ${args[1]}:/opt/analytics-generator/stacks tvd/modern-data-analytics-stack-generator:1.0
+   docker run --rm -v ${args[0]}:/tmp/custom-values.yml -v ${args[1]}:/opt/analytics-generator/stacks  --user $(id -u):$(id -g) trivadis/modern-data-platform-stack-generator:${args[2]}
    # Remove all empty lines at the end of the file
    sed -i -e :a -e '/^\n*$/{$d;N;ba' -e '}' ${args[1]}/**/docker-compose.yml
 else
-  echo "Usage : service-generator.sh [stack yml path] [output folder path]"
+  echo "Usage : service-generator.sh [custom yml path] [output folder path]"
 fi
 
 
