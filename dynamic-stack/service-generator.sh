372fb87a22b0e8504ede4247f72310dbfef158ba
Guido Schmutz <guido.schmutz@trivadis.com>
2019-12-16 15:32:22 +0100

    update sh script to remove empty lines at the end

--- dynamic-stack/service-generator.sh
+++ dynamic-stack/service-generator.sh
@@ -6,6 +6,8 @@ len=${#args[@]}
 if [ $len = 2 ] #2 arguments are required
 then
    docker run --rm -v ${args[0]}:/tmp/custom-values.yml -v ${args[1]}:/opt/analytics-generator/stacks tvd/modern-data-analytics-stack-generator:1.0
+   # Remove all empty lines at the end of the file
+   sed -i -e :a -e '/^\n*$/{$d;N;ba' -e '}' ${args[1]}/**/docker-compose.yml
 else
   echo "Usage : service-generator.sh [stack yml path] [output folder path]"
 fi
