c6205ca1e43d126e90931c95d8c94f0bf7da7596
Guido Schmutz <guido.schmutz@trivadis.com>
2020-01-02 14:24:27 +0100

    make sure the output folder is created, if it does not exist: #53

--- dynamic-stack/mdps-generate.sh
+++ dynamic-stack/mdps-generate.sh
@@ -6,6 +6,12 @@ len=${#args[@]}
 if [ $len = 3 ] #3 arguments are required
 then
    echo "Running the Modern Data Platform Stack Generator ...."
+
+   # check if the output folder exists, if not create it
+   if [ ! -d "${args[1]}" ]; then
+      mkdir "${args[1]}"
+   fi   
+   
    docker run --rm -v "${args[0]}":/tmp/custom-values.yml -v "${args[1]}":/opt/analytics-generator/stacks  --user $(id -u):$(id -g) trivadis/modern-data-platform-stack-generator:"${args[2]}"
    # Remove all empty lines at the end of the file
    sed -i '/^[[:space:]]*$/d' "${args[1]}/docker-compose.yml"
