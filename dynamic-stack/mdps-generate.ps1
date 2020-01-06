f27c5abf9d93758f71360c683dd049077b68b633
Luca Furrer <luca.furrer@trivadis.com>
2020-01-06 15:55:24 +0100

    Add powershell script to generate dynamic stack

---
+++ dynamic-stack/mdps-generate.ps1
@@ -0,0 +1,43 @@
+﻿<#
+
+.SYNOPSIS
+    Script to run the MDP Stack Generator with PowerShell
+
+.DESCRIPTION
+    This scrip runs the MDP Stack Generator. Docker has to be installed
+
+.PARAMETER defintion
+    custom values definition file
+
+.PARAMETER folder
+    Folder where the result should be deployed (has to exists)
+    
+
+.PARAMETER version
+    Version of the MDP Stack Generator used.
+    
+
+.EXAMPLE 
+    ./mdps-generate.ps1 -defintion '\config\custom.yml' -folder '\docker' -version "1.0.0"
+
+#>
+
+param(
+    [Parameter(Mandatory=$true, Position=0)]
+    [String]$defintion, 
+
+    [Parameter(Mandatory=$true, Position=1)]
+    [string]$folder, 
+
+    [Parameter(Mandatory=$true, Position=2)]
+    [String]$version
+)
+
+Write-Output "Running the Modern Data Platform Stack Generator ...."
+
+docker run --rm -v ('"{0}:/tmp/custom-values.yml"' -f $defintion) -v ('"{0}:/opt/analytics-generator/stacks"' -f $folder)  ("trivadis/modern-data-platform-stack-generator:{0}" -f $version)
+
+$dockerCompose = ("{0}/docker-compose.yml" -f  $folder)
+(Get-Content -Path $dockerCompose -Raw) -replace "(?s)`r`n\s*$" | Set-Content $dockerCompose
+
+Write-Output ('Modern Data Platform Stack generated successfully to {0}' -f  $folder)
