747ad3e6d22f7d763f1cbc0d1ab1a8c30e594f98
Guido Schmutz <guido.schmutz@trivadis.com>
2020-01-11 23:27:27 +0100

    Replace positional args with named arguments #72

--- dynamic-stack/mdps-generate.sh
+++ dynamic-stack/mdps-generate.sh
@@ -1,26 +1,78 @@
 #!/bin/bash
 
-args=("$@")
-len=${#args[@]}
+usage="Trivadis Modern Data Platform Stack Generator v1.0.0
 
-if [ "$len" = 3 ] #3 arguments are required
+Usage: $(basename "$0") [-h] [-d|--debug] [-cf|--configfile FILE] [-cr|--configref URL] [-o |--output PATH] [-v]--version version-number]
+
+generates the Docker-Compose stack
+
+    -h  	show this help text
+    -cf  	path to a local config file
+    -cr		URL of a remote config file 
+    -o 		path where the generator output should be placed in
+    -v  	version of the generator to use
+    -d 		add extra debug (verbose) output"
+		
+POSITIONAL=()
+while [[ $# -gt 0 ]]
+do
+key="$1"
+
+case $key in
+    -h|--help)
+    echo "$usage"
+    exit
+    ;;
+    -cf|--configfile)
+    CONFIG_FILE="$2"
+    shift # past argument
+    shift # past value
+    ;;
+    -cr|--configref)
+    CONFIG_REF="$2"
+    shift # past argument
+    shift # past value
+    ;;
+    -o|--output)
+    OUTPUT="$2"
+    shift # past argument
+    shift # past value
+    ;;
+    -v|--version)
+    VERSION="$2"
+    shift # past argument
+    shift # past value
+    ;;
+    -d|--debug)
+    DEBUG=1
+    shift # past argument
+    ;;
+    *)    # unknown option
+    POSITIONAL+=("$1") # save it in an array for later
+    shift # past argument
+    ;;
+esac	
+done
+set -- "${POSITIONAL[@]}" # restore positional parameters
+
+echo "Running the Modern Data Platform Stack Generator ...."
+
+if [ ${DEBUG:-0} -eq 1 ] 
 then
-   echo "Running the Modern Data Platform Stack Generator ...."
-
-   # check if the output folder exists, if not create it
-   if [ ! -d "${args[1]}" ]; then
-      mkdir "${args[1]}"
-   fi   
-   
-   docker run --rm -v "${args[0]}":/tmp/custom-values.yml -v "${args[1]}":/opt/analytics-generator/stacks  --user $(id -u):$(id -g) trivadis/modern-data-platform-stack-generator:"${args[2]}"
-
-   echo "Modern Data Platform Stack generated successfully to ${args[1]}"
-else
-  echo "mdps-generate version 1.0.0"
-  echo "Run the Modern Data Platform Stack Generator."
-  echo ""  
-  echo "Usage: "
-  echo "  mdps-generate [custom yml folder path] [output folder path] [mdp stack version]"
+   echo "configfile  = ${CONFIG_FILE}"
+   echo "configref   = ${CONFIG_REF}"
+   echo "output      = ${OUTPUT}"
+   echo "version     = ${VERSION}"
 fi
+
+# check if the output folder exists, if not create it
+if [ ! -d "${OUTPUT}" ]
+then
+   mkdir "${OUTPUT}"
+fi   
+   
+docker run --rm -v "${CONFIG_FILE}":/tmp/custom-values.yml -v "${OUTPUT}":/opt/analytics-generator/stacks -e CONFIG_REF="${CONFIG_REF}" --user $(id -u):$(id -g) trivadis/modern-data-platform-stack-generator:"${VERSION:-LATEST}"
+
+echo "Modern Data Platform Stack generated successfully to ${args[1]}"
 
 
