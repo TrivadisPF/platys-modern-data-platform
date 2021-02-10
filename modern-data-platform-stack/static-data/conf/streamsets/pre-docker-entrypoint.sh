aa275a61d92efa44b624ae1bc1d79e3a07033198
gschmutz <guido.schmutz@trivadis.com>
2021-02-10 08:44:48 +0100

    bug fix: #315

--- modern-data-platform-stack/static-data/conf/streamsets/pre-docker-entrypoint.sh
+++ modern-data-platform-stack/static-data/conf/streamsets/pre-docker-entrypoint.sh
@@ -9,7 +9,7 @@ echo
 set -e
 
 # Install dockerize
-ENV DOCKERIZE_VERSION v0.6.1
+DOCKERIZE_VERSION v0.6.1
 RUN sudo wget https://github.com/jwilder/dockerize/releases/download/$DOCKERIZE_VERSION/dockerize-linux-amd64-$DOCKERIZE_VERSION.tar.gz \
     && sudo tar -C /usr/local/bin -xzvf dockerize-linux-amd64-$DOCKERIZE_VERSION.tar.gz \
     && sudo rm dockerize-linux-amd64-$DOCKERIZE_VERSION.tar.gz
