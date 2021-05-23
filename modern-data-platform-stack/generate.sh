40e072c9be2dd17e934b8428d39a9096803494c5
gschmutz <guido.schmutz@trivadis.com>
2021-05-23 11:13:53 +0200

    Remove the CONFIG_URL parameter handling as the download is now done by platys: #344

--- modern-data-platform-stack/generate.sh
+++ modern-data-platform-stack/generate.sh
@@ -1,17 +1,5 @@
 #!/bin/sh
 
-# WGET the config reference from the URL configured by the CONFIG_URL environment variable
-if [ ${CONFIG_URL} ]
-then
-  if [ ${VERBOSE:-0} -eq 1 ]
-  then
-     echo "======================================================================"
-     echo "CONFIG_URL is set: downloading config.yml from ${CONFIG_URL}"
-     echo "======================================================================"
-  fi
-  wget ${CONFIG_URL} -O /tmp/config.yml
-fi
-
 cp -r /opt/mdps-gen/static-data/* /opt/mdps-gen/destination
 
 if [ ${VERBOSE:-0} -eq 1 ]
