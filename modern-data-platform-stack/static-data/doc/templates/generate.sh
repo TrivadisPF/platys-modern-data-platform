2ea9cbe8bf9cfe9a2e856fe6e589b8c426f3831e
gschmutz <guido.schmutz@trivadis.com>
2021-12-30 20:53:55 +0100

    replace dataplatform by PUBLIC_IP

--- modern-data-platform-stack/static-data/doc/templates/generate.sh
+++ modern-data-platform-stack/static-data/doc/templates/generate.sh
@@ -1,2 +1,5 @@
 jinja2 /templates/services.md.j2 /variables/docker-compose.yml --format=yaml --outfile /output/services.md
 jinja2 /templates/index.md.j2 /variables/config.yml --format=yaml --outfile /output/index.md
+
+cd /output
+find . -name "*.md" -exec sed -i 's/dataplatform:/'"$PUBLIC_IP"':/g' {} \;
