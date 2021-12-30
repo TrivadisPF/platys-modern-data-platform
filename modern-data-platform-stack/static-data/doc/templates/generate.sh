d7d42f24d9a403ed783162f08b6932b42441b36c
gschmutz <guido.schmutz@trivadis.com>
2021-12-30 22:29:44 +0100

    either use PUBLIC_IP or DOCKER_HOST_IP to replace all dataplatform: in md files: #420

--- modern-data-platform-stack/static-data/doc/templates/generate.sh
+++ modern-data-platform-stack/static-data/doc/templates/generate.sh
@@ -1,5 +1,13 @@
 jinja2 /templates/services.md.j2 /variables/docker-compose.yml --format=yaml --outfile /output/services.md
 jinja2 /templates/index.md.j2 /variables/config.yml --format=yaml --outfile /output/index.md
 
+# in all .md files, replace dataplatform: by the environment variable
 cd /output
-find . -name "*.md" -exec sed -i 's/dataplatform:/'"$PUBLIC_IP"':/g' {} \;
+
+
+if [ ${USE_PUBLIC_IP:-true} -eq "True" ]
+then
+	find . -name "*.md" -exec sed -i 's/dataplatform:/'"$PUBLIC_IP"':/g' {} \;
+else
+	find . -name "*.md" -exec sed -i 's/dataplatform:/'"$DOCKER_HOST_IP"':/g' {} \;
+fi	
