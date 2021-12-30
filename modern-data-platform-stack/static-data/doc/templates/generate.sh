ef18d85955769d099a051677ec3cdd0782b888e5
gschmutz <guido.schmutz@trivadis.com>
2021-12-30 23:01:58 +0100

    remove debug statements: #420

--- modern-data-platform-stack/static-data/doc/templates/generate.sh
+++ modern-data-platform-stack/static-data/doc/templates/generate.sh
@@ -3,9 +3,6 @@ jinja2 /templates/index.md.j2 /variables/config.yml --format=yaml --outfile /out
 
 # in all .md files, replace dataplatform: by the environment variable
 cd /output
-   echo "======================================================================"
-echo ${USE_PUBLIC_IP}
-   echo "======================================================================"
 
 if [ ${USE_PUBLIC_IP:-True} == "True" ]
 then
