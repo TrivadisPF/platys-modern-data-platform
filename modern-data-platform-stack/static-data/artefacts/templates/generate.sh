fe5ccae49fde53efea0487356bd9b4fad2e5dc7d
Guido Schmutz <guido.schmutz@trivadis.com>
2022-03-10 18:10:20 +0100

    bug fix for "generate.sh: applet not found"

--- modern-data-platform-stack/static-data/artefacts/templates/generate.sh
+++ modern-data-platform-stack/static-data/artefacts/templates/generate.sh
@@ -1,3 +1,5 @@
+#!/usr/bin/env sh
+
 jinja2 /templates/services.md.j2 /variables/docker-compose.yml --format=yaml --outfile /output/services.md
 jinja2 /templates/index.md.j2 /variables/docker-compose.yml --format=yaml --outfile /output/index.md
 
