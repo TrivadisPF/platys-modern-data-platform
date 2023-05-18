7f6848d7ade162a0377c44847002c666062e12b5
Guido Schmutz <guido.schmutz@trivadis.com>
2023-05-18 21:55:18 +0200

    Update docker-entrypoint-override.sh

--- modern-data-platform-stack/static-data/scripts/flink/docker-entrypoint-override.sh
+++ modern-data-platform-stack/static-data/scripts/flink/docker-entrypoint-override.sh
@@ -5,8 +5,8 @@ then
   /maven-download.sh central ${FLINK_INSTALL_JARS_PACKAGES} /opt/flink/lib
 fi
 
-if [ -z ${FLINK_DO_NOT_START+x} ]; then
-	tail -f /dev/null
-else
+if [ -z ${FLINK_DO_NOT_START} ]; then
 	/docker-entrypoint.sh $@
+else
+	tail -f /dev/null
 fi
