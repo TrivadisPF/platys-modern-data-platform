fb6575ee5f8f4cc6d54db3357aaa321116b58450
Guido Schmutz <guido.schmutz@trivadis.com>
2023-05-19 09:13:26 +0200

    update

--- modern-data-platform-stack/static-data/scripts/flink/docker-entrypoint-override.sh
+++ modern-data-platform-stack/static-data/scripts/flink/docker-entrypoint-override.sh
@@ -2,12 +2,12 @@
 
 if [ ${FLINK_INSTALL_MAVEN_DEP} ]
 then
-  /maven-download.sh central ${FLINK_INSTALL_MAVEN_DEP} /opt/flink/lib
+  /platys-scripts/maven-download.sh central ${FLINK_INSTALL_MAVEN_DEP} /opt/flink/lib
 fi
 
 if [ ${FLINK_INSTALL_FILE_DEP} ]
 then
-  /file-download.sh github ${FLINK_INSTALL_FILE_DEP} /opt/flink/lib
+  /platys-scripts/file-download.sh github ${FLINK_INSTALL_FILE_DEP} /opt/flink/lib
 fi
 
 if [ -z ${FLINK_DO_NOT_START} ]; then
