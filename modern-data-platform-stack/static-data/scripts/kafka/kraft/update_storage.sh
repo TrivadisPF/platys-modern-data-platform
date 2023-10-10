e7578844f79eb2bf422c830edfdd95f4fb2240a8
Guido Schmutz <guido.schmutz@trivadis.com>
2023-10-10 10:54:44 +0200

    update kraft + SCRAM: #733

--- modern-data-platform-stack/static-data/scripts/kafka/kraft/update_storage.sh
+++ modern-data-platform-stack/static-data/scripts/kafka/kraft/update_storage.sh
@@ -1,5 +1,19 @@
 #!/bin/sh
 
+# <===== START: copy from run script of confluent Dockerfile (https://github.com/confluentinc/kafka-images/blob/master/kafka/include/etc/confluent/docker/run)
+. /etc/confluent/docker/bash-config
+
+echo "===> User"
+id
+
+echo "===> Configuring ..."
+/etc/confluent/docker/configure
+
+echo "===> Running preflight checks ... "
+/etc/confluent/docker/ensure
+
+# ======> END: copy
+
 while getopts u:a:f: flag
 do
     case "${flag}" in
