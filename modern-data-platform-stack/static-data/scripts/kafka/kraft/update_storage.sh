c2e44ee4b2799d9ec7cd3760557b4ad4c12c425a
Guido Schmutz <guido.schmutz@trivadis.com>
2023-10-14 23:13:39 +0200

    update to support SCRAM 512: #161

--- modern-data-platform-stack/static-data/scripts/kafka/kraft/update_storage.sh
+++ modern-data-platform-stack/static-data/scripts/kafka/kraft/update_storage.sh
@@ -2,7 +2,8 @@
 
 CLUSTER_ID=${1}
 USERS_AND_PASSWORDS=${2-''}
-DEBUG=${3:-false}
+SASL_MECHANISM=${3}
+DEBUG=${4:-false}
 
 # we have to run configure from the original run command so that the /etc/kafka/kafa.properties is available for the kafka-storage command
 # <===== START: copy from run script of confluent Dockerfile (https://github.com/confluentinc/kafka-images/blob/master/kafka/include/etc/confluent/docker/run)
@@ -21,7 +22,7 @@ kafkaStorageCmd="kafka-storage format --config /etc/kafka/kafka.properties --ign
 export IFS=","
 for userAndPassword in $USERS_AND_PASSWORDS; do
   IFS=':' read -r -a data <<< "$userAndPassword"
-  kafkaStorageCmd+="--add-scram 'SCRAM-SHA-256=[name=${data[0]},password=${data[1]}]' "
+  kafkaStorageCmd+="--add-scram '$SASL_MECHANISM=[name=${data[0]},password=${data[1]}]' "
 done
 
 if $DEBUG ; then
