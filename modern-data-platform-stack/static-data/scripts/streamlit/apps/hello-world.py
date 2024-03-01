192309faf805e6242d389bf89cfbe72a7429bcf7
Guido Schmutz <guido.schmutz@trivadis.com>
2024-03-01 10:26:06 +0100

    update: #702

---
+++ modern-data-platform-stack/static-data/scripts/streamlit/apps/hello-world.py
@@ -0,0 +1,5 @@
+import streamlit as st
+
+st.title("Hi from streamlit inside Platys")
+st.write("Looks like this works properly.")
+st.write("This hello world streamlit application is located in ./scripts/streamlit/apps/hello-world.py")
