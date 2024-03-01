2bbef33bd38af6a34349b89add954e720584724e
Guido Schmutz <guido.schmutz@trivadis.com>
2024-03-01 10:39:37 +0100

    update: #702

--- modern-data-platform-stack/static-data/scripts/streamlit/run_pip_and_streamlit.sh
+++ modern-data-platform-stack/static-data/scripts/streamlit/run_pip_and_streamlit.sh
@@ -1,16 +1,13 @@
 #!/bin/sh
 
-apt update
-
 pip3 install --upgrade pip
-pip3 install --no-cache-dir -r streamlit
+pip3 install --no-cache-dir streamlit
 
 if [ -z "$2" ]
 then
   echo "No requirements.txt file configured, skipping PIP install phase!"
 else
   pip3 install --upgrade pip
-  pip3 install --no-cache-dir -r streamlit
   pip3 install --no-cache-dir -r $2
 fi
 
