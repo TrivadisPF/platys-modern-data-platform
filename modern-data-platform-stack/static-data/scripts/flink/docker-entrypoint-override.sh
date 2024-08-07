333b2b5dea8733459ef1164d50bb60ec65ee80fa
Guido Schmutz <guido.schmutz@trivadis.com>
2024-08-07 18:23:24 +0200

    Update docker-entrypoint-override.sh

--- modern-data-platform-stack/static-data/scripts/flink/docker-entrypoint-override.sh
+++ modern-data-platform-stack/static-data/scripts/flink/docker-entrypoint-override.sh
@@ -37,6 +37,19 @@ if [ "$METASTORE" != ":" ]; then
     sleep 1
   done;
   echo "Metastore is there!"
+  
+  # install hadoop dependencies - as shown here: https://www.decodable.co/blog/catalogs-in-flink-sql-hands-on#using-the-hive-catalog-with-flink-sql
+  mkdir -p ./lib/hive
+  curl https://repo1.maven.org/maven2/com/fasterxml/woodstox/woodstox-core/5.3.0/woodstox-core-5.3.0.jar -o ./lib/hive/woodstox-core-5.3.0.jar
+  curl https://repo1.maven.org/maven2/commons-logging/commons-logging/1.1.3/commons-logging-1.1.3.jar -o ./lib/hive/commons-logging-1.1.3.jar
+  curl https://repo1.maven.org/maven2/org/apache/commons/commons-configuration2/2.1.1/commons-configuration2-2.1.1.jar -o ./lib/hive/commons-configuration2-2.1.1.jar
+  curl https://repo1.maven.org/maven2/org/apache/hadoop/hadoop-auth/3.3.2/hadoop-auth-3.3.2.jar -o ./lib/hive/hadoop-auth-3.3.2.jar
+  curl https://repo1.maven.org/maven2/org/apache/hadoop/hadoop-common/3.3.2/hadoop-common-3.3.2.jar -o ./lib/hive/hadoop-common-3.3.2.jar
+  curl https://repo1.maven.org/maven2/org/apache/hadoop/hadoop-hdfs-client/3.3.2/hadoop-hdfs-client-3.3.2.jar -o ./lib/hive/hadoop-hdfs-client-3.3.2.jar
+  curl https://repo1.maven.org/maven2/org/apache/hadoop/hadoop-mapreduce-client-core/3.3.2/hadoop-mapreduce-client-core-3.3.2.jar -o ./lib/hive/hadoop-mapreduce-client-core-3.3.2.jar
+  curl https://repo1.maven.org/maven2/org/apache/hadoop/thirdparty/hadoop-shaded-guava/1.1.1/hadoop-shaded-guava-1.1.1.jar -o ./lib/hive/hadoop-shaded-guava-1.1.1.jar
+  curl https://repo1.maven.org/maven2/org/codehaus/woodstox/stax2-api/4.2.1/stax2-api-4.2.1.jar -o ./lib/hive/stax2-api-4.2.1.jar
+  
 fi
 
 echo "Start initialization"
