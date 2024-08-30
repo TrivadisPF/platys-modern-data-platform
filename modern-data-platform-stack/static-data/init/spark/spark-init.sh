b0c8b95455d022d1788cfbb25e5a98230db1975e
Guido Schmutz <guido.schmutz@trivadis.com>
2024-08-30 20:45:59 +0200

    Update spark-init.sh

--- modern-data-platform-stack/static-data/init/spark/spark-init.sh
+++ modern-data-platform-stack/static-data/init/spark/spark-init.sh
@@ -85,9 +85,9 @@ configure /etc/hadoop/httpfs-site.xml httpfs HTTPFS_CONF
 configure /etc/hadoop/kms-site.xml kms KMS_CONF
 #configure /etc/hadoop/mapred-site.xml mapred MAPRED_CONF
 
-configure_hive /spark/conf/hive-site.xml hive HIVE_SITE_CONF
+configure_hive /opt/bitnami/spark/conf/hive-site.xml hive HIVE_SITE_CONF
 
-configure_spark /spark/conf/spark-defaults.conf spark SPARK_DEFAULTS_CONF
+configure_spark /opt/bitnami/spark/conf/spark-defaults.conf spark SPARK_DEFAULTS_CONF
 
 if [ "$MULTIHOMED_NETWORK" = "1" ]; then
     echo "Configuring for multihomed network"
