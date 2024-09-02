160054581c05f00a1d402c95517dc82e3e595d72
Guido Schmutz <guido.schmutz@trivadis.com>
2024-09-02 19:50:41 +0200

    bug fix jupyter-init

--- modern-data-platform-stack/static-data/init/jupyter/jupyter-init.sh
+++ modern-data-platform-stack/static-data/init/jupyter/jupyter-init.sh
@@ -3,9 +3,8 @@
 # Removing JARs
 for jar in $(echo $REMOVE_JARS | sed "s/,/ /g")
 do
-      local JAR=$jar
-      rm /usr/local/spark/jars/$JAR
-      echo "/usr/local/spark/jars/$JAR has been removed!"
+      rm /usr/local/spark/jars/$jar
+      echo "/usr/local/spark/jars/$jar has been removed!"
 done
 
 # Downloading Maven dependencies
