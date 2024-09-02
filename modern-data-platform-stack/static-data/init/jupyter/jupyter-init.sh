e4658e9b8896112dd23fb6c0e6020d54c1af958b
Guido Schmutz <guido.schmutz@trivadis.com>
2024-09-02 15:49:42 +0200

    update removing jars for jupyter

--- modern-data-platform-stack/static-data/init/jupyter/jupyter-init.sh
+++ modern-data-platform-stack/static-data/init/jupyter/jupyter-init.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
 
+# Removing JARs
+for jar in $(echo $REMOVE_JARS | sed "s/,/ /g")
+do
+      local JAR=$jar
+      rm /usr/local/spark/jars/$JAR
+      echo "/usr/local/spark/jars/$JAR has been removed!"
+done
+
+# Downloading Maven dependencies
 /maven-download.sh central ${MAVEN_DOWNLOAD_JARS} /usr/local/spark/jars
