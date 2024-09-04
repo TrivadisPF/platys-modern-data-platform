068fc09a3a01443f2a6a03463b33ee328aeae486
Guido Schmutz <guido.schmutz@trivadis.com>
2024-09-04 14:49:30 +0200

    only run the maven download if the spark directory exists

--- modern-data-platform-stack/static-data/init/jupyter/jupyter-init.sh
+++ modern-data-platform-stack/static-data/init/jupyter/jupyter-init.sh
@@ -3,9 +3,11 @@
 # Removing JARs
 for jar in $(echo $REMOVE_JARS | sed "s/,/ /g")
 do
-      echo "Trying to remove /usr/local/spark/jars/$jar"
-      rm -fv /usr/local/spark/jars/$jar
+    echo "Trying to remove /usr/local/spark/jars/$jar"
+    rm -fv /usr/local/spark/jars/$jar
 done
 
 # Downloading Maven dependencies
-/maven-download.sh central ${MAVEN_DOWNLOAD_JARS} /usr/local/spark/jars
+if [ -d "/usr/local/spark/jars" ]; then
+	/maven-download.sh central ${MAVEN_DOWNLOAD_JARS} /usr/local/spark/jars
+fi
