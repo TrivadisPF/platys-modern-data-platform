de32bd615af64291e262bebdda0fec44f84ad7fb
Guido Schmutz <schmutz68@gmail.com>
2024-11-06 21:01:54 +0100

    Update dataverse_init_solr.sh

--- modern-data-platform-stack/static-data/init/dataverse/solr/dataverse_init_solr.sh
+++ modern-data-platform-stack/static-data/init/dataverse/solr/dataverse_init_solr.sh
@@ -1,9 +1,33 @@
 #!/bin/bash
 #
-# Initialize SOLR for Dataverse by copying the metafiles
+# Initialize SOLR for Dataverse by creating a ckan core
+# Arguments are supplied via environment variables: DATAVERSE_CORE_NAME
+# Example:
+#   DATAVERSE_CORE_NAME=collection1
 
 set -e
 
-cp conf/* /template/
+# Set some defaults as documented
+DATAVERSE_CORE_NAME=${DATAVERSE_CORE_NAME:-"collection1"}
 
-echo "SOLR initialized"
+echo "Check whether SOLR is initialized for Dataverse"
+COREDIR="$SOLR_HOME/$DATAVERSE_CORE_NAME"
+if [ -d "$COREDIR" ]; then
+    echo "SOLR already initialized, skipping initialization"
+else
+    echo "Initializing SOLR core $DATAVERSE_CORE_NAME for Dataverse"
+    # init script for handling an empty /var/solr
+    /opt/solr/docker/scripts/init-var-solr
+    
+    # Precreate CKAN core
+    /opt/solr/docker/scripts/precreate-core $DATAVERSE_CORE_NAME
+    
+    # Remove the managed schema and replace it with the Dataverse metadata
+    echo "Removing the managed schema"
+    rm $COREDIR/conf/managed-schema.xml
+
+    echo "Adding Dataverse schema and solrconfig"
+    cp -R /docker-entrypoint-initdb.d/conf/* $COREDIR/conf
+    
+    echo "SOLR initialized"
+fi
