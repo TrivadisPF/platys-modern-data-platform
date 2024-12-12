75ca09a24819526bea1f02608d30d81aa0ef628c
Guido Schmutz <schmutz68@gmail.com>
2024-12-12 21:20:20 +0100

    Update run_pip_and_python.sh

--- modern-data-platform-stack/static-data/scripts/python/run_pip_and_python.sh
+++ modern-data-platform-stack/static-data/scripts/python/run_pip_and_python.sh
@@ -4,7 +4,7 @@ apk update
 
 if [ -z "$2" ]
 then
-  echo "No requirements.txt file configured, skipping PIP install phase!"
+  echo "No requirements.txt file configured, skipping PIP install phase!"
 else
   pip3 install --upgrade pip
   pip3 install --no-cache-dir -r $2
@@ -12,8 +12,8 @@ fi
 
 if [ -z "$1" ]
 then
-  echo "No python script configured, just running python without an application!"
-  python
+  echo "No python script configured, just running python without an application!"
+  sleep infinity
 else
   python $1
 fi  
